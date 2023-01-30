b3bc6f498af32fb79751e942215eea025ba42c07
Trentbn <trentnelson97@gmail.com>
2023-01-30 16:39:44 -0700

    Added date to header. Fixed footer. Polishing

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -8,3 +8,11 @@ x.onclick = toggleMenu;
 
 document.getElementById('currentyear').innerHTML = new Date().getFullYear();
 document.getElementById('lastupdated').innerHTML = new Date(document.lastModified);
+
+///document.getElementById("headerDate").innerHTML = new Date();///
+const dateField = document.getElementById("headerDate");
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+dateField.innerHTML = `<em>${fulldateUK}</em>`;
