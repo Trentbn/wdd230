6d5eb356950bacc8659581dba4e97b817983623b
Trentbn <trentnelson97@gmail.com>
2023-03-13 15:17:15 -0600

    Worked on last vistited counter/minor changes

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -3,4 +3,18 @@ let daysSince = Number(window.localStorage.getItem("lastVisited-ls"));
 let now = Date.now();
 let findDaysSince = now;
 
-if (daysSince)
+if (daysSince !== 0)
+{
+    findDaysSince -= daysSince;
+    findDaysSince = Math.round(daysSince / (1000*60*60*24))
+ 
+    if (findDaysSince > 1)
+        {
+            document.getElementById("visits").textContent = "${findDaysSince} days ago";
+        }
+}
+else
+{
+    document.getElementById("visits").textContent = "today";
+}
+localStorage.setItem("lastVisted-ls", now);
