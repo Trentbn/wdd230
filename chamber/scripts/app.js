cfedf9c080e68e66869db688cc6534d678f80f4a
Trentbn <trentnelson97@gmail.com>
2023-03-21 12:43:56 -0600

    fixed errors

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -30,5 +30,5 @@ dateField.innerHTML = `<em>${fulldateUK}</em>`;
     }
     else banner.style.display = "none";
 
-document.getElementById("date&time").textContent = fulldateUK;
+// document.getElementById("date&time").textContent = fulldateUK;
 
